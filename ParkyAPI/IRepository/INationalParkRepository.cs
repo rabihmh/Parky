0c103f3ef0db8aece03fa04c3ac7cb25bec30f46
rabihmh <76842351+rabihmh@users.noreply.github.com>
2024-02-15 15:23:04 +0200

    Add Repository

---
+++ ParkyAPI/IRepository/INationalParkRepository.cs
@@ -0,0 +1,22 @@
+﻿using ParkyAPI.Models;
+
+namespace ParkyAPI.IRepository
+{
+    public interface INationalParkRepository
+    {
+        public ICollection<NationalPark> GetNationalParks();
+
+        public NationalPark GetNationalPark(int nationalParkId);
+
+        public bool NationalParkExists(string name);
+        public bool NationalParkExists(int id);
+
+        public bool CreateNationalPark(NationalPark nationalPark);
+
+        public bool UpdateNationalPark(NationalPark nationalPark);
+
+        public bool DeleteNationalPark(NationalPark nationalPark);
+
+        public bool Save();
+    }
+}
--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ParkyAPI.Data;
+using ParkyAPI.IRepository;
 
 namespace ParkyAPI
 {
@@ -16,6 +17,8 @@ namespace ParkyAPI
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
                 builder.Configuration.GetConnectionString("DefaultConnection")
                 ));
+
+            builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
